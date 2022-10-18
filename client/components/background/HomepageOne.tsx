eedfc01c438d078b8f022dc41cc6ccb652fd8335
Chloe <chloe.baker@huler.io>
2022-10-18 15:52:58 +0100

    adding policy content

---
+++ client/components/background/HomepageOne.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+interface Props {
+  className?: string;
+}
+
+const HomepageOne = ({ className }: Props) => (
+  <svg
+    className={className}
+    viewBox="0 0 234 92"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M234 30.5355C199.122 11.0884 158.977 0 116.295 0C74.1863 0 34.5477 10.7822 0 29.7348V90.6912C32.2476 65.9312 72.58 51.2 116.295 51.2C160.649 51.2 201.522 66.3655 234 91.7846V30.5355Z"
+      fill="url(#paint0_linear)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear"
+        x1="-3.18127e-07"
+        y1="92"
+        x2="234"
+        y2="94.5"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#E7ECFE" />
+        <stop offset="0.633811" stopColor="#9E91F7" />
+        <stop offset="1" stopColor="#ECA8FE" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+HomepageOne.defaultProps = {
+  className: "",
+};
+
+const StyledHomepageOne = styled(HomepageOne)`
+  position: absolute;
+  width: 150vw;
+  top: -2%;
+  left: -25vw;
+  z-index: -1;
+  transform: rotate(-5deg);
+
+  @media (min-width: 500) {
+    width: 100%;
+    left: 0;
+    top: -10%;
+  }
+
+  @media (min-width: 1000) {
+    top: -25%;
+  }
+`;
+
+export default StyledHomepageOne;
--- client/pages/policies.tsx
+++ client/pages/policies.tsx
@@ -12,6 +12,36 @@ const Policies = ({ className }: PolicyProps) => {
     <div className={className}>
       <ContainerWrapper>
         <h1>Policies</h1>
+        <figure>
+          <strong>Overview</strong>
+          <p>
+            Our holiday year runs from 1st January to 31st December, the
+            standard entitlement is 25 days plus bank holidays.
+          </p>
+          <p>
+            When an employee joins/leaves the company part way through the year,
+            they will be notified of their pro-rata annual leave entitlement.
+          </p>
+          <p>
+            Around September time, you’ll receive an update on how much holiday
+            each of your team has left to book.
+          </p>
+          <strong>Staff employed during covid-19 (2020)</strong>
+          <p>
+            In line with government guidance, staff employed throughout 2020 who
+            were unable to use all of their annual leave entitlement due to
+            covid-19 are able to roll their unused 2020 days into 2021 and 2022.
+            Only unused days from 2020 can be carried forward, staff will not be
+            able to carry forward any unused days from your standard 2021
+            entitlement.{" "}
+          </p>
+          <p>
+            As an additional benefit in 2021, staff who feel they will be unable
+            to make use of all their holiday entitlement or would prefer to
+            exchange these for the equivalent monetary value will be able to
+            sell up to 5 days back to the company.
+          </p>
+        </figure>
         <Button primaryOutline href="/">
           Back
         </Button>
@@ -30,23 +60,56 @@ const StyledPolicies = styled(Policies)`
   position: absolute;
   top: 0;
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   z-index: -5;
   overflow: hidden;
 
   h1 {
     margin: 0;
     position: relative;
-    top: 1rem;
-    padding-bottom: 3rem;
+    top: 2rem;
+    padding-bottom: 2rem;
     line-height: 1.15;
     font-size: 3.5rem;
     color: #fb6666;
     text-align: center;
   }
   button {
-    margin-top: 40%;
+    position: absolute;
     left: 2rem;
+    bottom: 2rem;
+  }
+  figure {
+    position: relative;
+    width: 70%;
+    height: 50%;
+    margin: auto;
+    text-align: center;
+    top: 3rem;
+    margin-bottom: 2rem;
+    overflow-y: scroll;
+    padding-right: 5px;
+
+    ::-webkit-scrollbar {
+      width: 7px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1;
+      border-radius: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: #888;
+      border-radius: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background: #555;
+    }
+    p {
+      text-align: left;
+    }
   }
 `;
 export default StyledPolicies;
