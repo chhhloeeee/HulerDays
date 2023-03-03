f581bc80b35a46b0e5ec5cb6475552707ea630cb
Chloe <chloe.baker@huler.io>
2023-03-03 16:07:30 +0000

    feature: login basic form set up

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { Formik } from "formik";
 import styled from "styled-components";
 import AboutTwo from "./background/AboutTwo";
 import Button from "./Button";
@@ -7,12 +8,62 @@ interface LoginFormProps {
   className?: string;
 }
 
+interface Values {
+  email: string | number;
+  password: string | number | readonly string[];
+}
+
 const LoginForm = ({ className }: LoginFormProps) => {
   return (
     <div className={className}>
       <Logo />
       <h1>HulerDays</h1>
       <div>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validateOnMount
+          onSubmit={(values: Values, { setSubmitting, resetForm }) => {
+            //handleChangePassword(values, setSubmitting);
+            setSubmitting(true);
+            setTimeout(() => {
+              resetForm();
+            }, 400);
+          }}
+        >
+          {({ handleSubmit, handleBlur, handleChange, values }) => (
+            <div>
+              <form
+                className="form"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleSubmit();
+                }}
+              >
+                <input
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  required
+                />
+                <input
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  required
+                />
+              </form>
+            </div>
+          )}
+        </Formik>
         <Button primary href="/home">
           Login
         </Button>
@@ -51,11 +102,10 @@ const StyledLoginForm = styled(LoginForm)`
     top: 30%;
     margin: auto;
     border-radius: 15px;
-    background-color: #fff;
+
     z-index: ${(props) => props.theme.zLayers.default};
     button {
       left: 40%;
-      top: 80%;
     }
   }
   ${AboutTwo} {
@@ -63,6 +113,43 @@ const StyledLoginForm = styled(LoginForm)`
     transform: rotate(170deg);
     top: -28rem;
     position: relative;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    width: 100%;
+    position: relative;
+    padding: 0 5px;
+    padding-top: 3rem;
+    input {
+      border-radius: 50px;
+      padding: 20px;
+      border: 3px solid #c1c7d0;
+      flex: 1;
+      outline: none;
+      font-family: sofia-pro, sans-serif;
+      font-size: 14px;
+      font-weight: bold;
+      margin-bottom: 26px;
+      color: #c1c7d0;
+
+      @media screen and (max-width: ${(props) =>
+          props.theme.breakpoints.small}) {
+        padding: 8px 16px;
+      }
+
+      &::placeholder {
+        font-family: sofia-pro, sans-serif;
+        color: #c1c7d0;
+      }
+      &:active,
+      &:focus {
+      }
+      @media only screen and (max-width: 500px) {
+        font-size: 16px;
+      }
+    }
   }
 `;
 export default StyledLoginForm;
