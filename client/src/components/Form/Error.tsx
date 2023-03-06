8a1189a4fc542cc37fc0efa4ecc709bc6cb51f0d
Chloe <chloe.baker@huler.io>
2023-03-06 12:15:11 +0000

    fix: add yup and errors to login

---
+++ client/src/components/Form/Error.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+interface ErrorProps {
+  attached?: boolean;
+}
+
+export const Error = styled.span<ErrorProps>`
+  color: #fff;
+  display: inline-block;
+  width: 90%;
+  margin: 20px auto;
+  padding: 15px;
+  border-radius: 20px;
+  font-size: 14px;
+  line-height: 21px;
+  position: relative;
+  background-color: var(--primary-color);
+  ${(props) =>
+    props.attached &&
+    `
+      width: 80%;
+      position: absolute;
+      top: calc(100% - 26px);
+      left: 0;
+      font-size: 12px;
+      margin: 0 auto;
+      z-index: ${props.theme.zLayers.overlay};
+      border-radius: 0 0 20px 20px;
+      padding: 10px;
+      left: 0;
+      right: 0;
+    `}
+`;
--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -4,17 +4,39 @@ import styled from "styled-components";
 import AboutTwo from "./background/AboutTwo";
 import Button from "./Button";
 import Logo from "./Logo";
+import { Error } from "./form/Error";
+import * as Yup from "yup";
 
 interface LoginFormProps {
   className?: string;
 }
 
 interface Values {
-  email: string | number;
-  password: string | number | readonly string[];
+  email: string;
+  password: string;
 }
 
 const LoginForm = ({ className }: LoginFormProps) => {
+  const FormWrapper = styled.div`
+    position: relative;
+    width: 700px;
+    height: 300px;
+    margin: auto;
+    border-radius: 15px;
+
+    z-index: ${(props) => props.theme.zLayers.default};
+    button {
+      position: absolute;
+      top: 90%;
+      left: 40%;
+    }
+  `;
+  const LoginSchema = Yup.object().shape({
+    password: Yup.string()
+      .min(8, "Your Password must be longer than 8 characters")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+  });
   const router = useRouter();
   return (
     <div className={className}>
@@ -26,6 +48,7 @@ const LoginForm = ({ className }: LoginFormProps) => {
             email: "",
             password: "",
           }}
+          validationSchema={LoginSchema}
           validateOnMount
           onSubmit={(values: Values, { setSubmitting, resetForm }) => {
             console.log("here");
@@ -37,38 +60,54 @@ const LoginForm = ({ className }: LoginFormProps) => {
             }, 400);
           }}
         >
-          {({ handleSubmit, handleBlur, handleChange, values }) => (
-            <div>
+          {({
+            handleSubmit,
+            handleBlur,
+            handleChange,
+            values,
+            errors,
+            touched,
+          }) => (
+            <FormWrapper>
               <form
-                className="form"
                 onSubmit={(e) => {
                   e.preventDefault();
                   handleSubmit();
                 }}
               >
-                <input
-                  placeholder="Email"
-                  name="email"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required
-                />
-                <input
-                  placeholder="Password"
-                  name="password"
-                  type="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  required
-                />
+                <div>
+                  <input
+                    placeholder="Email"
+                    name="email"
+                    type="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    required
+                  />
+                  {!!(errors.email && touched.email) && (
+                    <Error attached>{errors.email}</Error>
+                  )}
+                </div>
+                <div>
+                  <input
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    required
+                  />
+                  {!!(errors.password && touched.password) && (
+                    <Error attached>{errors.password}</Error>
+                  )}
+                </div>
               </form>
               <Button primary type="submit" onClick={() => handleSubmit()}>
                 Login
               </Button>
-            </div>
+            </FormWrapper>
           )}
         </Formik>
       </div>
@@ -99,19 +138,6 @@ const StyledLoginForm = styled(LoginForm)`
     top: 1rem;
   }
 
-  div:nth-of-type(2) {
-    position: relative;
-    width: 700px;
-    height: 300px;
-    top: 30%;
-    margin: auto;
-    border-radius: 15px;
-
-    z-index: ${(props) => props.theme.zLayers.default};
-    button {
-      left: 40%;
-    }
-  }
   ${AboutTwo} {
     z-index: 0;
     transform: rotate(170deg);
@@ -119,39 +145,43 @@ const StyledLoginForm = styled(LoginForm)`
     position: relative;
   }
   form {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    width: 100%;
-    position: relative;
-    padding: 0 5px;
-    padding-top: 3rem;
-    input {
-      border-radius: 50px;
-      padding: 20px;
-      border: 3px solid #c1c7d0;
-      flex: 1;
-      outline: none;
-      font-family: sofia-pro, sans-serif;
-      font-size: 14px;
-      font-weight: bold;
-      margin-bottom: 26px;
-      color: #c1c7d0;
-
-      @media screen and (max-width: ${(props) =>
-          props.theme.breakpoints.small}) {
-        padding: 8px 16px;
-      }
-
-      &::placeholder {
+    div {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      width: 100%;
+      position: relative;
+      padding: 0 5px;
+      input {
+        border-radius: 50px;
+        padding: 20px;
+        border: 3px solid #c1c7d0;
+        flex: 1;
+        outline: none;
         font-family: sofia-pro, sans-serif;
+        font-size: 14px;
+        font-weight: bold;
+        margin-bottom: 26px;
         color: #c1c7d0;
-      }
-      &:active,
-      &:focus {
-      }
-      @media only screen and (max-width: 500px) {
-        font-size: 16px;
+        &:last-of-type {
+          margin-top: 26px;
+        }
+
+        @media screen and (max-width: ${(props) =>
+            props.theme.breakpoints.small}) {
+          padding: 8px 16px;
+        }
+
+        &::placeholder {
+          font-family: sofia-pro, sans-serif;
+          color: #c1c7d0;
+        }
+        &:active,
+        &:focus {
+        }
+        @media only screen and (max-width: 500px) {
+          font-size: 16px;
+        }
       }
     }
   }
