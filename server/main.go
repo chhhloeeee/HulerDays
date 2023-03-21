1004bcdff2e38d88371ccba23216e9a4968c4d3d
Chloe <chloe.baker@huler.io>
2023-03-21 11:51:36 +0000

    set up main.go

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+
+	"github.com/HulerDays/controller"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/getUsers", controller.AllUsers).Methods("GET")
+	router.HandleFunc("/insertUser", controller.InsertUser).Methods("POST")
+	http.Handle("/", router)
+	fmt.Println("Connected to port 1234")
+	log.Fatal(http.ListenAndServe(":1234", router))
+}
